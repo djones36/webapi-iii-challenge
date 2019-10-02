2cb039ead5b864814b7c7f84d563c7718b67a597
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-02 13:04:01 -0700

    logger finished

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 const server = require('./server')
 const PORT = 8000
 
-server.listing(PORT, () => console.log(`Running on port${PORT}`))
+server.listen(PORT, () => console.log(`Running on port ${PORT}`))
--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const server = express();
 
+//logger here
+server.use(logger)// <= logger
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+server.use(errorHandler)
+
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`${req.method} to ${req.path} at ${new Date().toISOString()}`)
+  next()
 };
+
+function errorHandler(err, req, res, next) {
+  console.log(err)
+}
 
 module.exports = server;
